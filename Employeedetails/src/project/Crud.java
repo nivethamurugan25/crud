dfd6b31ba9e2cb9f8044b8258c46cc576a840c3a
whitehouse computer <whitehouse computer@whitehousecompu>
2022-06-07 19:45:27 +0530

    I shared 1 file

---
+++ Employeedetails/src/project/Crud.java
@@ -0,0 +1,94 @@
+package project;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Crud 
+{
+	    public void create() {
+		try{ Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","Nivethamurugan@25"); 
+		Statement st=con.createStatement(); 
+		st.executeUpdate(("insert into employeedetails values(103,'Ajay','ajay@gmail.com','Manager',60000)"));
+		con.close();
+		} 
+		catch(Exception e)
+		{ 
+			System.out.println(e.toString());
+		}
+		}   
+	
+	
+	    public void read() {
+		try{ Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","Nivethamurugan@25"); 
+		Statement st=con.createStatement(); 
+		ResultSet rs=st.executeQuery("select * from employeedetails");
+		while(rs.next())
+		{
+		   System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getString(4)+"\t"+rs.getString(5)); 
+		} 
+		} 
+		catch(Exception e)
+		{ 
+			System.out.println(e.toString());
+		}
+	    }
+	    
+	    
+	    public void update() {
+	    try{ Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","Nivethamurugan@25"); 
+	    Statement st=con.createStatement(); 
+		st.executeUpdate("update employeedetails set salary= salary+20000 where eid=101"); 		
+	    con.close(); 
+	    } 
+	    catch(Exception e)
+		{ 
+	       System.out.println(e.toString());
+		}
+	    }
+	    
+	    public void delete() {
+	    try{ Class.forName("com.mysql.jdbc.Driver");
+	    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","Nivethamurugan@25"); 
+	    Statement st=con.createStatement(); 
+		st.executeUpdate("delete from employeedetails where eid=102"); 		
+		con.close(); 
+		} 
+		catch(Exception e)
+		{ 
+		   System.out.println(e.toString());
+		}
+		}
+	
+	public static void main(String args[])
+	{
+		  int a;
+		   System.out.println("Select an option");
+		   Scanner sc=new Scanner(System.in);
+		   a=sc.nextInt();
+		   Crud c=new Crud();
+		   switch(a) {
+		   case 1:
+		         System.out.println("Create");
+		          c.create();
+		         break;
+		   case 2:
+		         System.out.println("read");
+		         c.read();
+		         break;
+		   case 3:
+		         System.out.println("update");
+		         c.update();
+		         break;
+		   case 4:
+		         System.out.println("delete");
+		         c.delete();
+		         break;
+		  default:
+			     System.out.println("please select the correct option");
+		         break;
+	}
+	}
+}
